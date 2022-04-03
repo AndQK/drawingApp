01ac450b3bd253a65c25a0bc80b96d755e036ecd
Antonio <antonio.skondras@aalto.fi>
2022-04-03 19:54:24 +0300

    Added two changers for my comboBoxes. Added one ActionListener for my clear button as well.

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,11 +1,11 @@
+import java.awt.event.ActionListener
 import scala.swing._
 import scala.collection.mutable._
-import scala.swing.event.{MouseDragged, MousePressed, MouseReleased}
 
 object GUI extends SimpleSwingApplication {
 
  // STRINGS for drop down boxes
-  val colours = Seq("Black", "Red", "Green", "Orange", "Yellow", "Purple", "Brown", "Blue")
+  val colours = Seq("Black", "Red", "Green", "Orange", "Yellow", "Brown", "Blue", "Pink", "Magenta", "Cyan", "Gray", "Dark_gray", "Lime", "Violet", "Salmon", "Gold")
   val shapes  = Seq("Circle", "Rectangle", "Ellipse", "Line")
 
   // Creates the Frame with buttons
@@ -19,23 +19,49 @@ object GUI extends SimpleSwingApplication {
     preferredSize = new Dimension(700, 700)
     maximumSize   = new Dimension(700, 700)
 
-
-    val drawingScreen: Component = new DrawSpace
+    // DrawSpace instance for drawing
+    val drawingScreen = new DrawSpace
     drawingScreen.preferredSize = new Dimension(500, 500)
     drawingScreen.minimumSize = new Dimension(500, 500)
     drawingScreen.maximumSize = new Dimension(500, 500)
 
+    //drop-down list for colours
+    val colorList = new ComboBox(colours)
+    // drop-down list for shapes
+    val shapeList = new ComboBox(shapes)
+
+    // action listener for colorList
+    val colorChanger: ActionListener = new ColorChanger(drawingScreen, colorList)
+    colorList.peer.addActionListener(colorChanger)
+
+    // action listener for shapeList
+    val shapeChanger: ActionListener = new ShapeChanger(drawingScreen, shapeList)
+    shapeList.peer.addActionListener(shapeChanger)
+
+
+    // buttons for different actions
+    val PenBtn = new Button("Pen")
+
+    val undoBtn = new Button("Undo")
+
+    val textBtn = new Button("Text")
+
+    val loadBtn = new Button("Load")
+
+    val clearBtn = new Button("Clear")
+    clearBtn.peer.addActionListener(e => drawingScreen.clear())
+    val saveBtn = new Button("Save")
 
     // buttons which we use to switch between settings
     val buttons = new FlowPanel {
-      contents += new ComboBox(colours)
-      contents += new ComboBox(shapes)
-      contents += new Button("Pen")
-      contents += new Button("undo")
-      contents += new Button("text")
-      contents += new Button("choose")
-      contents += new Button("Clear")
-      contents += new Button("save")
+      contents += colorList
+      contents += shapeList
+      contents += PenBtn
+      contents += undoBtn
+      contents += textBtn
+      contents += loadBtn
+      contents += clearBtn
+      contents += saveBtn
     }
     val drawPanel = new FlowPanel {
       contents += drawingScreen
