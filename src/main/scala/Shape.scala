92018f691e0711e332cf81ae762390f394521bda
Antonio <antonio.skondras@aalto.fi>
2022-04-20 08:22:08 +0300

    Added new value (group) to the shape class.

--- src/main/scala/Shape.scala
+++ src/main/scala/Shape.scala
@@ -1,4 +1,4 @@
 import java.awt.Color
 
-class Shape(val oldX: Int, val oldY: Int, val currentX: Int, val currentY: Int, val color: Color, val shape: String)  {
+class Shape(val oldX: Int, val oldY: Int, val currentX: Int, val currentY: Int, val color: Color, val shape: String, val group: Int)  {
 }
